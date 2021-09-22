b99c28dc31c8628fe583ee93f005cfda18dd9ae7
rpraneeth264 <70067528+rpraneeth264@users.noreply.github.com>
2021-09-22 15:11:23 +0530

    Create DemoProg.py

---
+++ DemoProg.py
@@ -0,0 +1,14 @@
+n1=int(input("enter the first number:"))
+n2=int(input("enter the second number:"))
+sum=n1+n2
+sub=n1-n2
+prod=n1*n2
+quot=n1//n2
+rem=n1%n2
+div=n1/n2
+print("sum of",n1,",",n2,"is",sum)
+print("subtraction of",n1,",",n2,"is",sub)
+print("product of",n1,",",n2,"is",prod)
+print("quotient in division of ",n1,",",n2,"is",quot)
+print("remainder in division of",n1,",",n2,"is",rem)
+print("perfect division of",n1,",",n2,"is",div)
